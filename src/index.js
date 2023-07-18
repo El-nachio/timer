62401274361f01d5adb5703e9b6ed54a9461a648
Ignacio <igna-232011@hotmail.com>
2023-07-18 20:05:08 -0300

    html structure
    we added the html structure as the freecodecamp cuorse requires

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
+/* eslint-disable */
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    render() {
+        return (
+            <div>
+                <div>
+                    <label id='break-label'>break-time</label>
+                    <button id='break-increment'></button>
+                    <div value='5'>value of the break-time(change it)</div>
+                    <button id='break-decrement'></button>
+                </div>
+                <div>
+                    <div id='Timer-label'>WORK(Timer-label)</div>
+                    <div id='time-left'>this display the time</div>
+                </div>
+                <div className='running-buttons'>
+                    <a id='start-stop'>play</a>
+                    <a id='reset'>reset</a>
+                </div>
+                <div>
+                    <label id='session-label'>session-time</label>
+                    <button id='session-increment'></button>
+                    <div value='25'>value of the session-time(change it)</div>
+                    <button id='session-decrement'></button>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Timer />, document.getElementById('root'));
