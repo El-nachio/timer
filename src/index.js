947e7a59fc08c5ab2bdc59485bb88925fe3680aa
Ignacio <igna-232011@hotmail.com>
2023-07-20 20:11:11 -0300

    changes
    i added colors to the buttons and trying to make responsive to the page

--- src/index.js
+++ src/index.js
@@ -5,32 +5,34 @@ import './index.css';
 
 class Timer extends React.Component {
     constructor(props) {
-        super(props);
-        
+        super(props);   
     }
-
     render() {
         return (
-            <div>
+            <div className='timer-panel'>
                 <div>
-                    <label id='break-label'>break-time</label>
-                    <button id='break-increment'></button>
-                    <div value='5'>value of the break-time(change it)</div>
-                    <button id='break-decrement'></button>
-                </div>
-                <div>
-                    <div id='Timer-label'>WORK(Timer-label)</div>
-                    <div id='time-left'>this display the time</div>
-                </div>
-                <div className='running-buttons'>
-                    <a id='start-stop'>play</a>
-                    <a id='reset'>reset</a>
-                </div>
-                <div>
-                    <label id='session-label'>session-time</label>
-                    <button id='session-increment'></button>
-                    <div value='25'>value of the session-time(change it)</div>
-                    <button id='session-decrement'></button>
+                    <div>
+                        <div id='Timer-label'>WORK(Timer-label)</div>
+                        <div id='time-left'>this display the time</div>
+                    </div>
+                    <div className='running-buttons'>
+                        <a id='start-stop'>play</a>
+                        <a id='reset'>reset</a>
+                    </div>
+                    <div className='button-seccion'>
+                        <div className='click-label'>
+                            <label id='break-label'>rest-time</label>
+                            <div id='break-length' value='5'>value of the break-time(change it)</div>
+                            <button id='break-increment'></button>
+                            <button id='break-decrement'></button>
+                        </div>
+                        <div className='click-label'>
+                            <label id='session-label'>work-time</label>
+                            <div id='session-length' value='25'>value of the session-time(change it)</div>
+                            <button id='session-increment'></button>
+                            <button id='session-decrement'></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
